5b183153056a8683130a79251c08045d192d03aa
ashish1206 <ashishchauhan1206@gmail.com>
2021-05-09 14:08:43 +0530

    ball partial moves added

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,11 @@
 #include<sys/ioctl.h>
 #include<time.h>
 #include<string.h>
+#include<chrono>
 #define REF_LEN 4
 using namespace std;
 
-void updateGrid();
+void updateReflector();
 
 struct termios orgTermios;
 struct winsize ws;
@@ -19,6 +20,11 @@ int row;
 int col;
 char **bricks;
 int refX;
+int ballVelX;
+int ballVelY;
+int ballPosX;
+int ballPosY;
+auto ball_last_time = chrono::high_resolution_clock::now();
 
 void error(const char *msg){
     write(STDOUT_FILENO, "\x1b[2J", 4);
@@ -27,11 +33,11 @@ void error(const char *msg){
     exit(1);
 }
 
+//Ternimal
 void disableRawMode(){
     if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orgTermios) == -1)
         error("tcsetattr error");
-    //enable cursor
-    write(STDOUT_FILENO, "\e[?25h", 6);
+    write(STDOUT_FILENO, "\x1b[?25h", 6);
 }
 
 void enableRawMode(){
@@ -43,17 +49,14 @@ void enableRawMode(){
     rawTermios.c_oflag &= ~(OPOST);
     rawTermios.c_cflag |= (CS8);
     rawTermios.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    rawTermios.c_cc[VMIN] = 0;
+    rawTermios.c_cc[VTIME] = 5;
     if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &rawTermios) == -1)
         error("tcsetattr error");
-    //hide cursor
-    write(STDOUT_FILENO, "\e[?25l", 6);
+    write(STDOUT_FILENO, "\x1b[?25l", 6);
 }
 
-void refershScreen(){
-    write(STDOUT_FILENO, "\x1b[2J", 4);
-    write(STDOUT_FILENO, "\x1b[H", 3);
-}
-
+//Key processing
 char readKeyPress(){
     int nread;
     char c;
@@ -75,12 +78,12 @@ void processKeyPress(){
         case 'a':
             refX--;
             refX = (refX<0?0:refX);
-            updateGrid();
+            updateReflector();
             break;
         case 'd':
             refX++;
             refX = (refX+REF_LEN>col?col-REF_LEN:refX);
-            updateGrid();
+            updateReflector();
             break;
     }
 }
@@ -91,7 +94,68 @@ void getWindowSize(){
     }
 }
 
-void updateGrid(){
+//Grid gen and update
+long long millisSinceBallLastMove(){
+    auto now_time = chrono::high_resolution_clock::now();
+    long long diff_in_millis = chrono::duration_cast<chrono::microseconds>(now_time - ball_last_time).count();
+    return diff_in_millis;
+}
+
+void moveBall(){
+    long long milli = millisSinceBallLastMove();
+    if(milli >= 1000){
+        int tempX = ballPosX + ballVelX;
+        int tempY = ballPosY + ballVelY;
+        if(tempX==0 || tempY==0 || tempX==row-1 || tempY==col-1){
+            if(tempX == 0){
+                ballVelX = 1;
+                tempX = 0;
+            }
+            if(tempY == 0){
+                ballVelY = 1;
+                tempY = 0;
+            }
+            if(tempX == row-1){
+                ballVelX = -1;
+                tempX = row-1;
+            }
+            if(tempY == col){
+                ballVelY = -1;
+                tempY = col-1;
+            }
+        }
+        else if(bricks[tempY][tempX-1] != ' '){
+            ballVelX = 1;
+            bricks[tempY][tempX-1] = ' ';
+        }
+        else if(bricks[tempY-1][tempX] != ' '){
+            ballVelY = 1;
+            bricks[tempY-1][tempX] = ' ';
+        }
+        else if(bricks[tempY][tempX+1] != ' '){
+            ballVelX = -1;
+            bricks[tempY][tempX+1] = ' ';
+        }
+        else if(bricks[tempY+1][tempX] != ' '){
+            ballVelY = -1;
+            bricks[tempY+1][tempX] = ' ';
+        }
+        // else if(bricks[tempY][tempX] != ' '){
+        //     ballVelX = -1*ballVelX;
+        //     ballVelY = -1*ballVelY;
+        //     bricks[tempY][tempX] = ' ';
+        //     tempX = ballPosX;
+        //     tempY = ballPosY;
+        // }
+        bricks[ballPosY][ballPosX] = ' ';
+        ballPosX = tempX;
+        ballPosY = tempY;
+        bricks[ballPosY][ballPosX] = 'O';
+        ball_last_time = chrono::high_resolution_clock::now();
+    }
+}
+
+void updateReflector(){
     for(int i=0;i<col;i++){
         if(i>=refX && i<=refX+REF_LEN){
             bricks[row-1][i] = '=';
@@ -131,6 +195,11 @@ char** generateGrid(){
     for(int i=refX;i<=refX + REF_LEN;i++){
         bricks[row-1][i] = '=';
     }
+    ballVelX = -1;
+    ballVelY = -1;
+    ballPosY = row-2;
+    ballPosX = refX+2;
+    bricks[ballPosY][ballPosX] = 'O';
     return bricks;
 }
 
@@ -139,14 +208,21 @@ void renderGrid(){
     memcpy(render, bricks[0], col);
     int renderLen = col;
     for(int i=1;i<row;i++){
-        char *temp = (char *)realloc(render, renderLen+col+2);
-        memcpy(&temp[renderLen], "\r\n", 2);
-        memcpy(&temp[renderLen+2], bricks[i], col);
-        renderLen += col+2;
+        char *temp = (char *)realloc(render, renderLen+col+5);
+        memcpy(&temp[renderLen], "\x1b[K", 3);
+        memcpy(&temp[renderLen+3], "\r\n", 2);
+        memcpy(&temp[renderLen+5], bricks[i], col);
+        renderLen += col+5;
         render = temp;
     }
     write(STDOUT_FILENO, render, renderLen);
     free(render);
+}
+
+void refreshScreen(){
+    moveBall();
+    renderGrid();
+    write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
 int main(){
@@ -154,8 +230,7 @@ int main(){
     getWindowSize();
     bricks = generateGrid();
     while(true){
-        refershScreen();
-        renderGrid();
+        refreshScreen();
         processKeyPress();
     }
     return 0;
